beaa38acc60f553be22392556863da1b2e7fdf52
topleftstyll <joshua.brackett@hotmail.ca>
2022-06-25 09:54:44 -0400

    devise set up with jwt token

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users,
+              controllers: {
+                  sessions: 'users/sessions',
+                  registrations: 'users/registrations'
+              }
+              
   namespace :api do
     namespace :v1 do
       resources :children
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
