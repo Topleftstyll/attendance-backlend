a7e01c97d0eb3e7757541888bd5683124ea9050c
topleftstyll <joshua.brackett@hotmail.ca>
2022-06-29 20:56:04 -0400

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,26 @@ require 'faker'
 #     Api::V1::Group.create(name: name, user_id: 1, description: Faker::Restaurant.description)
 # end
 
-Api::V1::Group.all.each do |group|
-    teacher = Api::V1::Teacher.create(
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        user_id: 1,
-        group_id: group.id
-    )
-    group.update_attribute(:teacher_id, teacher.id)
-    rand(1..10).times do |i|
-        Api::V1::Child.create(
-            first_name: Faker::Name.first_name,
-            last_name: Faker::Name.last_name,
-            teacher_id: teacher.id,
-            group_id: group.id,
-            attendance_id: 1
-        )
-    end
-end
+# Api::V1::Group.all.each do |group|
+#     teacher = Api::V1::Teacher.create(
+#         first_name: Faker::Name.first_name,
+#         last_name: Faker::Name.last_name,
+#         user_id: 1,
+#         group_id: group.id
+#     )
+#     group.update_attribute(:teacher_id, teacher.id)
+#     rand(1..10).times do |i|
+#         Api::V1::Child.create(
+#             first_name: Faker::Name.first_name,
+#             last_name: Faker::Name.last_name,
+#             teacher_id: teacher.id,
+#             group_id: group.id,
+#             attendance_id: 1
+#         )
+#     end
+# end
+
+# trying to make a merge conflict
 
 # Api::V1::Child.all.each do |child|
 #     rand(1..10).times do |i|
